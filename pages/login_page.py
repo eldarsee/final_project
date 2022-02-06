2123f81766ec1ba4a65873b7bf14e684e6138c59
outofw <eldar.se1997@gmail.com>
2022-02-06 18:22:58 +0300

    LoginPage class was implemented

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'login' in self.browser.current_url, "it's not login page"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+        'there is no login form on the page'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+        'there is no register form on the page'
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,3 +14,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+'''def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()'''
